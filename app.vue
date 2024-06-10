66cae349572171ba626a74c8eb73e6fdb1def5bd
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-10 15:21:41 +0300

    chore: config shadcnui

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+            'rounded bg-zinc-100 min-h-12 text-2xl dark:bg-zinc-900 border border-zinc-200 dark:border-zinc-700 px-3 lg:px-4',
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+});
+
