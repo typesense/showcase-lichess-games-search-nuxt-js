40d8cc08d51cf3309de11374f9ca67dad8c5e861
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-13 17:08:26 +0300

    build: add runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,12 @@ export default defineNuxtConfig({
   colorMode: {
     classSuffix: "",
   },
+  runtimeConfig: {
+    public: {
+      PUBLIC_TYPESENSE_SEARCH_ONLY: process.env.TYPESENSE_READ_ONLY_API_KEY,
+      PUBLIC_TYPESENSE_HOST: process.env.PUBLIC_TYPESENSE_HOST,
+      PUBLIC_TYPESENSE_PORT: process.env.PUBLIC_TYPESENSE_PORT,
+      PUBLIC_TYPESENSE_PROTOCOL: process.env.PUBLIC_TYPESENSE_PROTOCOL,
+    },
+  },
 });
