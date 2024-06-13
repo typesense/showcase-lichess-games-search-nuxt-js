3f3bb2836314e99cb7ec7a472a4a73f6885ef849
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-06-13 17:08:52 +0300

    feat: add global types for process env

---
+++ types/globals.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    PUBLIC_TYPESENSE_HOST: string;
+    PUBLIC_TYPESENSE_PORT: string;
+    PUBLIC_TYPESENSE_PROTOCOL: string;
+    TYPESENSE_ADMIN_API_KEY: string;
+    TYPESENSE_READ_ONLY_API_KEY: string;
+  }
+}
